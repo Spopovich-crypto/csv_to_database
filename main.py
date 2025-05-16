8c4353de0563853d3e8f3b08ed66699494a00f3d
tmk <morimoto.ke@gmail.com>
2025-05-16 13:25:33 +0900

    Refactor search_csv_file function to enhance file searching logic and improve readability

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import polars as pl
 from pathlib import Path
 
+
+def search_csv_file(target_path: Path, file_name_pattern: list[str]) -> list[Path]:
+    target_csv_files = []
+
+    csv_files = target_path.rglob("*.csv")
+
+    if not csv_files:
+        print(f"No CSV files found in {target_path}")
+        return target_csv_files
+    if len(file_name_pattern) == 0:
+        target_csv_files = list(csv_files)
+        return target_csv_files
+    else:
+        for cscv_file in csv_files:
+            if any(pat in cscv_file.name for pat in file_name_pattern):
+                target_csv_files.append(cscv_file)
+        return target_csv_files
+
+
+
 def read_pi_file(file_path: Path, encoding="utf-8"):
     # ── 1. ヘッダー読み込み（3行） ─────────────────────
     with open(file_path, encoding=encoding) as f:
