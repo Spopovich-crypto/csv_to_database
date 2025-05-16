310806f77e8e8a95f85eff906ce4e2e2071c12b7
tmk <morimoto.ke@gmail.com>
2025-05-16 12:27:51 +0900

    Implement CSV file search functionality in main.py

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+from pathlib import Path
+
+
+
+
+def search_csv_file(target_path: Path, file_name_pattern: list[str]) -> list[Path]:
+    target_csv_files = []
+
+    csv_files = target_path.rglob("*.csv")
+
+    if not csv_files:
+        print(f"No CSV files found in {target_path}")
+        return target_csv_files
+    if len(file_name_pattern) == 0:
+        target_csv_files = list(csv_files)
+        return target_csv_files
+    else:
+        for cscv_file in csv_files:
+            if any(pat in cscv_file.name for pat in file_name_pattern):
+                target_csv_files.append(cscv_file)
+        return target_csv_files
+
 def main():
     print("Hello from csv-to-database!")
 
